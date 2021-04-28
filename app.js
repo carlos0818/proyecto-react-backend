eff913c72deaa22773d4ec784d136d4e231c62c5
Carlos Benavides <praimus666@gmail.com>
2021-04-28 17:29:39 -0500

    Implementando los métodos para listar, crear, votar a favor y votar en contra

--- app.js
+++ app.js
@@ -28,32 +28,61 @@ const Post = sequelize.import("./models/posts.js");
 // Happy Path: returns all posts in an array in JSON format (Status 200)
 // Sad Path: None
 app.get("/posts", function(req, res){
-
+    Post.findAll({ order: [['points', 'DESC']]}).then(function(posts){
+        res.send(JSON.stringify(posts));
+    });
 });
 
 // Create a post
 // Happy Path: creates the post item (Status 201 - returns copy of created post)
 // Sad Path: none
 app.post("/posts", function(req, res){
+    const post = {
+        title: req.body.title,
+        url: req.body.url
+    };
 
+    Post.create(post).then(function(post){
+        res.status(201).send('Created post')
+    }).catch(function(error){
+        //res.send(error);
+    })
 });
 
 // Upvote a post
 // Happy Path: upvote a post (Status 204 - empty JSON)
 // Sad Path: post does not exist (Status 404 - empty JSON)
 app.patch("/posts/:id/upvote", function(req, res){
-
+    Post.findByPk(req.params.id).then(function(posts){
+        if(posts != null){
+            posts.points = posts.points + 1
+            posts.save()
+            res.status(204).send('');
+        }
+    }).catch(function(error){
+        res.status(404).send('')
+    })
 });
 
 // Downvote a post
 // Happy Path: downvote a post (Status 204 - empty JSON)
 // Sad Path: post does not exist (Status 404 - empty JSON)
 app.patch("/posts/:id/downvote", function(req, res){
-
+    Post.findByPk(req.params.id).then(function(posts){
+        if(posts != null && posts.points > 0){
+            posts.points = posts.points - 1
+            posts.save()
+            res.status(204).send('');
+        } else {
+            res.status(204).send('');
+        }
+    }).catch(function(error){
+        res.status(404).send('')
+    })
 });
 
 // STOP: Don't change anything below this line
 
 app.listen(PORT, function () {
-    console.log("Server started...")
+    console.log("Server started..." + PORT)
 });
